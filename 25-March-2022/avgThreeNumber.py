2b332e1cd5a75b5ff08e96843b860fed5feddd5a
Dechapon Tongmak <dechapon.t@gmail.com>
2022-03-25 21:47:12 +0700

    Implement find average in python

---
+++ 25-March-2022/avgThreeNumber.py
@@ -0,0 +1,9 @@
+num1 = float(input('Enter num 1: '))
+print("Your input is", num1)
+num2 = float(input('Enter num 2: '))
+print("Your input is", num2)
+num3 = float(input('Enter num 3: '))
+print("Your input is", num3)
+
+avg = (num1 + num2 + num3)/3
+print("Your avg is", avg)
